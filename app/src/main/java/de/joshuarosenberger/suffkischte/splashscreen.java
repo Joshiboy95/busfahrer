b89e18d9f9eb0aa2d7a59c1a07fc24db91e79a78
et1902 <43827504+et1902@users.noreply.github.com>
2018-10-18 19:27:42 +0200

    Changed Colour

--- app/src/main/java/de/joshuarosenberger/suffkischte/splashscreen.java
+++ app/src/main/java/de/joshuarosenberger/suffkischte/splashscreen.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class splashscreen extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 1500;
+    private static int SPLASH_TIME_OUT = 1200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
